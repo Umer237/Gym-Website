6373055596b83ce7e069ef9e5fe37aea7d1c1302
Umer237 <umerafzal981@gmail.com>
2023-02-25 15:54:58 +0500

    update

---
+++ src/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+import { FaFacebookF } from "react-icons/fa";
+import {BsTwitter } from "react-icons/bs";
+
+const Header = () => {
+  return (
+   <>
+   <div className='Header-Flex'>
+   <h1>GYM</h1>
+   <div>
+   <ul>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Classes</a></li>
+    <li><a href="/">Blog</a></li>
+    <li><a href="/">Contact</a></li>
+   </ul>
+   </div>
+   <div className='Header-Icons'>
+   
+   <FaFacebookF className='Header-Icon-1'/>
+   
+   <BsTwitter className='Header-Icon-2'/>
+   </div>
+   </div>
+   </>
+  )
+}
+
+export default Header
