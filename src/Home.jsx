ce77b85d3d2bb402350d2f4dc165379997805889
Umer237 <umerafzal981@gmail.com>
2023-02-27 12:51:41 +0500

    umer

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner'
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
+import Section5 from './Section5'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
     <Section2/>
     <Section3/>
     <Section4/>
+    <Section5/>
     </>
   )
 }
---
+++ src/Section5.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Section5.css'
+import Exercise1 from './Images/Yoga.png'
+import Person1 from './Images/Egypt-Women.jpeg'
+import Exercise2 from'./Images/Cycling.png'
+import Exercise3 from './Images/Lifting.png'
+
+const Section5 = () => {
+  return (
+    <>
+    <div className="Section-5-Text">
+    <h2>GYM CLASSES</h2>
+    <h1>Regular Workouts Improve Cardiac Perfomance</h1>
+    <p>The sky was cloudless and of a deep dark blue. The spectacle before us was indeed sublime. </p>
+    </div>
+    <div className='Section-5-Flex'>
+      <div className='Section-1'>
+     <img className='Exercise1' src={Exercise1} alt="" />
+     <h1>Yoga</h1>
+     <div className='Person1-Flex'>
+      <img className='Person1' src={Person1} alt="" />
+      <p>Lisanne Viscaal</p>
+      </div>
+      </div>
+      <div className='Section-2'>
+     <img className='Exercise2' src={Exercise2} alt="" />
+     <h1>Cycling</h1>
+     <div className='Person2-Flex'>
+      <img className='Person1' src={Person1} alt="" />
+      <p>Lisanne Viscaal</p>
+      </div>
+      </div>
+      <div className='Section-3'>
+     <img className='Exercise3' src={Exercise3} alt="" />
+     <h1>Weight Lifting</h1>
+     <div className='Person3-Flex'>
+      <img className='Person1' src={Person1} alt="" />
+      <p>Lisanne Viscaal</p>
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Section5
