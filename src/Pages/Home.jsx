3a291bc9877cfbf43fa8bf6c5e074b3aa7736783
Umer237 <umerafzal981@gmail.com>
2023-03-07 14:53:05 +0500

    umer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,6 +10,7 @@ import Section7 from './Section7'
 import Section8 from './Section8'
 import Section9 from './Section9'
 import Section10 from './Section10'
+import Section11 from './Section11'
 import Footer from './Footer'
 
 const Home = () => {
@@ -26,6 +27,7 @@ const Home = () => {
     <Section8/>
     <Section9/>
     <Section10/>
+    <Section11/>
     <Footer/>
     </>
   )
---
+++ src/Pages/Section11.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Section11.css'
+const Section11 = () => {
+  return (
+<>
+<div className="Section-11-Flex">
+    <div>
+<h2>TESTIMONIALS</h2>
+<h1>What Our Clients Say About Us</h1>
+<p>The sky was cloudless and of a deep dark blue. The spectacle before us was indeed sublime.</p>
+</div>
+<div className='Section-1'>
+    <div className='Section-1-Text'>
+    <img className='Person-1' src={man} alt="" />
+    <h2>David Salsa</h2>
+    <h3>Park Street, L/k-2, USA</h3>
+    </div>
+    <p>There are many variations of passages of Lorem ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which slightly believable.</p>
+</div>
+<div className='Section-2'>
+    <div className='Section-2-Text'>
+    <img className='Person-2' src={girl} alt="" />
+    <h2>Lusia Salsa</h2>
+    <h3>Park Street, L/k-2, USA</h3>
+    </div>
+    <p>There are many variations of passages of Lorem ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which slightly believable.</p>
+</div>
+</div>
+</>
+  )
+}
+
+export default Section11
