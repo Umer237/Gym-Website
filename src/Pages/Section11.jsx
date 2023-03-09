601ec110ca44666e7d0affb19bb80635f3f23263
Umer237 <123937455+Umer237@users.noreply.github.com>
2023-03-09 12:24:03 +0500

    umer

--- src/Pages/Section11.jsx
+++ src/Pages/Section11.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import './Section11.css'
-import girl1 from './Images/girl.jpg'
-import man1 from './Images/man.jpg'
+import React from "react";
+import "./Section11.css";
+import girl1 from "./Images/girl.jpg";
+import man1 from "./Images/man.jpg";
 const Section11 = () => {
   return (
-<>
-<div className="Section-11-Flex">
-    <div>
-<h2>TESTIMONIALS</h2>
-<h1>What Our Clients Say About Us</h1>
-<p>The sky was cloudless and of a deep dark blue. The spectacle before us was indeed sublime.</p>
-</div>
-<div className='Section-1'>
-    <div className='Section-1-Text'>
-    <img className='Person-1' src={man1} alt="" />
-    <h2>David Salsa</h2>
-    <h3>Park Street, L/k-2, USA</h3>
-    </div>
-    <p>There are many variations of passages of Lorem ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which slightly believable.</p>
-</div>
-<div className='Section-2'>
-    <div className='Section-2-Text'>
-    <img className='Person-2' src={girl1} alt="" />
-    <h2>Lusia Salsa</h2>
-    <h3>Park Street, L/k-2, USA</h3>
-    </div>
-    <p>There are many variations of passages of Lorem ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which slightly believable.</p>
-</div>
-</div>
-</>
-  )
-}
+    <>
+      <div className="Section-11-Flex">
+        <div className="Side-Text">
+          <h2>TESTIMONIALS</h2>
+          <h1>What Our Clients Say About Us</h1>
+          <p>
+            The sky was cloudless and of a deep dark blue. The spectacle before
+            us was indeed sublime.
+          </p>
+        </div>
+        <div className="Section-1">
+          <div className="Section-1-Text">
+            <img className="Person-1" src={man1} alt="" />
+            <h2>Ekene Obasey </h2>
+            <p>
+          Formed a great hindrance to my speed resolved contary to my first.
+            </p>
+          </div>
+        </div>
+        <div className="Section-2">
+          <div className="Section-2-Text">
+            <img className="Person-2" src={girl1} alt="" />
+            <h2>Sanee Viscal</h2>
+          </div>
+          <p>Formed a great hindrance to my speed resolved contary to my first.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Section11
+export default Section11;
